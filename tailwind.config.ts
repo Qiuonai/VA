99b0ab2ffb7cbe48c89dad7f96ab45f21da27170
Quion <178565459+Qiuonai@users.noreply.github.com>
2026-05-12 13:50:04 +0600

    Updated tailwind
    Updated tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,16 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2563eb',
+      },
+    },
   },
   plugins: [],
 }
